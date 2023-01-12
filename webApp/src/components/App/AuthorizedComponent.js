2999c5528cbac569390c6116bafb0926031dfddb
pijio <m2495aw@yandex.ru>
2023-01-12 22:13:26 +0600

    Исправил деструктуризацию

--- webApp/src/components/App/AuthorizedComponent.js
+++ webApp/src/components/App/AuthorizedComponent.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import useAuth from "../../hooks/useAuth/useAuth";
 import { Redirect } from 'react-router-dom';
 import {useSelector} from "react-redux";
 
 const AuthorizedComponent = Component => props => {
-    const { isAuth } = useSelector(({auth}) => auth.isAuthed)
+    const { isAuth } = useSelector(({auth}) => auth)
     if( isAuth )
         return <Component {...props}/>;
     return <Redirect to={'/auth'}/>
